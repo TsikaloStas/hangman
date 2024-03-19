08ebd7e02ecedac2eade7d31e98230f9f9a34f10
TsikaloStas <stascikalo@gmail.com>
2024-03-19 21:28:21 +0300

    Translated on english and fixed variables

--- main.py
+++ main.py
@@ -1,68 +1,80 @@
 import random
 
 def hangman():
-    print('Добро пожаловать в игру "Виселица"!')
+    print('Welcome to the Hangman game!')
 
-    dict_words = {'цвет': ['красный', 'синий', 'жёлтый', 'зелёный'],
-                  'работа': ['врач', 'столяр', 'учитель', 'продавец'],
-                  'фигура': ['треугольник', 'круг', 'квадрат'],
-                  'еда': ['бургер', 'макароны', 'суп', 'каша']}
+    dict_words = {'color': ['red', 'blue', 'yellow', 'green'],
+                  'job': ['doctor', 'carpenter', 'teacher', 'salesman'],
+                  'figure': ['triangle', 'circle', 'square'],
+                  'food': ['burger', 'pasta', 'soup', 'porridge']}
 
+    # select a random category from the dictionary
     random_key = random.choice(list(dict_words))
-    random_lists_v = dict_words.get(random_key)
-    print(f'Вы угадываете слово в категории - {random_key}')
 
-    magic_word = random.choice(random_lists_v)
-    new_list = ['_' for _ in range(len(magic_word))]
+    # generate a list of words from this category
+    list_magic_words = dict_words.get(random_key)
+    print(f'Your word from the category - {random_key}')
 
-    lst_mag_w = [char for char in magic_word if magic_word.count(char) == 1]
-    el = random.choice(lst_mag_w)
+    # choose a magic word
+    magic_word = random.choice(list_magic_words)
+    enigma_word = ['_' for _ in range(len(magic_word))]
 
-    for i, k in enumerate(new_list):
+    # pick a random open letter
+    random_letter = [char for char in magic_word if magic_word.count(char) == 1]
+    el = random.choice(random_letter)
+
+    # replace an empty value in the riddle word with an open letter
+    for i, k in enumerate(enigma_word):
         for j, v in enumerate(magic_word):
             if el == magic_word[j]:
                 if i == j:
-                    new_list[i] = el
+                    enigma_word[i] = el
 
     n = len(magic_word)
-    print(f'Количество попыток на угадывание слова - {n}')
+    print(f'Number of attempts to guess a word - {n}')
 
+    # tally counter
     counter = 0
     win_flag = True
 
     while win_flag:
-        print('Ваше загаданное слово: ', *new_list)
-        element = input(f"Ваша {counter+1} попытка: ")
+        print('Your target word: ', *enigma_word)
+        element = input(f"Your {counter+1} attempt: ")
 
+        # if letters in the word = 1, find the index and replace the letter by position
         if magic_word.count(element) == 1:
-            indx = magic_word.index(element)
-            new_list[indx] = element
+            index = magic_word.index(element)
+            enigma_word[index] = element
             counter += 1
-            print(f'Ура, вы угадали букву {element}')
+            print(f'Yay, you guessed the letter {element}')
 
+        # if letters in the word > 1, find all indexes of such letter and write them to the list
         elif magic_word.count(element) > 1:
-            lst_ = []
+            index_list = []
 
             for i, v in enumerate(magic_word):
                 if v == element:
-                    lst_.append(i)
+                    index_list.append(i)
 
-            for z in lst_:
-                new_list[z] = magic_word[z]
+            for z in index_list:
+                enigma_word[z] = magic_word[z]
 
             counter += 1
-            print(f'Ура, вы угадали букву {element}')
+            print(f'Yay, you guessed the letter {element}')
+
         else:
             counter += 1
-            print('Упс! Вы не угадали...')
+            print('Oops! You guessed wrong.....')
 
-        if '_' not in new_list:
-            print(f'Поздравляем. Вы угадали слово - {magic_word}\U0001F917 Ураааа!!!')
+        # if there are no blanks left in the riddled word - report victory
+        if '_' not in enigma_word:
+            print(f'Congratulations. You guessed the word - {magic_word}\U0001F917 Yay!!!')
             win_flag = False
 
+        # prescribe a situation where we guess a word on the last try
         if counter == n:
-            if new_list.count('_') != 0:
-                print('К сожалению, вы проиграли. Количество попыток закончилось.. Вас повесили\U0001F62A')
+            if enigma_word.count('_') != 0:
+                print('Unfortunately, you\'ve lost. The number of attempts has expired.. You have been hanged\U0001F62A')
                 win_flag = False
 
 hangman()
