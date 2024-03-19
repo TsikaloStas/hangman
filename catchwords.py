c61789fdf8adb6b31973e265ccf8054106369d61
TsikaloStas <stascikalo@gmail.com>
2024-03-19 21:38:33 +0300

    A dictionary of random words

---
+++ catchwords.py
@@ -0,0 +1,12 @@
+dict_words = {
+    'animals': ['dog', 'cat', 'lion', 'elephant', 'zebra', 'giraffe', 'monkey', 'tiger', 'panda', 'koala', 'bear', 'wolf', 'fox'],
+    'colors': ['red', 'blue', 'yellow', 'green', 'orange', 'purple', 'pink', 'brown', 'black', 'white', 'gray', 'cyan', 'magenta'],
+    'fruits': ['apple', 'banana', 'orange', 'grape', 'pear', 'strawberry', 'kiwi', 'mango', 'pineapple', 'watermelon', 'cherry', 'plum', 'lemon'],
+    'countries': ['USA', 'Canada', 'UK', 'Australia', 'Germany', 'France', 'Italy', 'Japan', 'China', 'India', 'Brazil', 'Russia', 'Mexico'],
+    'sports': ['football', 'basketball', 'soccer', 'tennis', 'golf', 'cricket', 'swimming', 'boxing', 'volleyball', 'rugby', 'baseball', 'hockey', 'athletics'],
+    'vehicles': ['car', 'bus', 'train', 'airplane', 'helicopter', 'bicycle', 'motorcycle', 'boat', 'truck', 'scooter', 'submarine', 'rocket', 'hovercraft'],
+    'jobs': ['doctor', 'teacher', 'engineer', 'lawyer', 'chef', 'police', 'firefighter', 'pilot', 'nurse', 'scientist', 'artist', 'writer', 'builder'],
+    'music': ['rock', 'pop', 'jazz', 'hip-hop', 'classical', 'blues', 'country', 'reggae', 'electronic', 'rap', 'folk', 'metal', 'indie'],
+    'body_parts': ['head', 'arm', 'leg', 'hand', 'foot', 'ear', 'eye', 'nose', 'mouth', 'shoulder', 'knee', 'toe', 'fingers'],
+    'planets': ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune', 'Pluto', 'Moon', 'Sun', 'Io', 'Europa']
+}
